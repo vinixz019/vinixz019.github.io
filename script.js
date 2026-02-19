dce94071021636a7ab65a4b7feaf84487bde4d62
vinixz019 <vnisbackvv@gmail.com>
2026-02-19 08:09:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-let carrinho = [];
-let total = 0;
 
-function adicionarCarrinho(nome, preco) {
-  carrinho.push({ nome, preco });
-  total += preco;
-  atualizarCarrinho();
-}
-
-function atualizarCarrinho() {
-  const itens = document.getElementById("itensCarrinho");
-  itens.innerHTML = "";
-  carrinho.forEach(item => {
-    itens.innerHTML += `<li>${item.nome} — R$ ${item.preco}</li>`;
-  });
-  document.getElementById("total").textContent = total;
-}
